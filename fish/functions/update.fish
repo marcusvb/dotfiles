3068697b2e5b22a4a96804046a61e042847229d3
Marcus van Bergen <marcusvanbergen@live.com>
2017-01-11 07:07:39 +0100

    adding brew cu and updating update.fish

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -3,7 +3,9 @@ function update
     sudo softwareupdate -i -a
     brew update
     brew upgrade --force --all
+    brew cu
     brew cleanup
+    brew doctor
     npm install npm -g
     npm update -g
     apm update
