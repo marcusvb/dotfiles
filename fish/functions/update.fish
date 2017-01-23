75d37d62a2bcd133f5d737edf1a52ac85b8ac521
Marcus van Bergen <marcusvanbergen@live.com>
2017-01-23 12:05:23 +0100

    updates and depcop

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -2,10 +2,12 @@
 function update
     sudo softwareupdate -i -a
     brew update
-    brew upgrade --force --all
+    brew upgrade --force
     brew cu
     brew cleanup
+    brew cask cleanup
     brew doctor
+    brew cask doctor
     npm install npm -g
     npm update -g
     apm update
