e46242157083a1f7fc6bf9b8ae0f4eba241fa3f3
Marcus van Bergen <marcusvanbergen@live.com>
2017-11-24 15:14:38 +0100

    Adding some features to HS for the killing and spawning of Pb desktop because it's buggy.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,8 +3,20 @@ hs.timer = require("hs.timer")
 function caffeinateWatcher(eventType)
     if (eventType == hs.caffeinate.watcher.screensDidUnlock) then
         print ("unlocked")
-        hs.timer.usleep(3000000)
+        hs.timer.usleep(1000000)
         hs.execute("open -a KeePassXC")
+    end
+    -- For restarting PB for desktop after sleep because it's momentarilly very
+    -- buggy.
+    if (eventType == hs.caffeinate.watcher.screensDidWake) then
+        print("staring PB")
+        hs.execute([[open --background -a  PB\ for\ Desktop]])
+    end
+    -- After screen sleep we kill PB because we'll start it in background
+    -- after screen on
+    if (eventType == hs.caffeinate.watcher.screensDidSleep) then
+        print("Killing PB")
+        hs.execute([[pkill PB\ for\ Desktop]])
     end
 end
 
