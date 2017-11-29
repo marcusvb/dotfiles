02815cae7f050fa40021dc041f653f68486a196e
Marcus van Bergen <marcusvanbergen@live.com>
2017-11-29 18:04:03 +0100

    Small changes and mount keepass command

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,16 +8,16 @@ function caffeinateWatcher(eventType)
     end
     -- For restarting PB for desktop after sleep because it's momentarilly very
     -- buggy.
-    if (eventType == hs.caffeinate.watcher.screensDidWake) then
-        print("staring PB")
-        hs.execute([[open --background -a  PB\ for\ Desktop]])
-    end
-    -- After screen sleep we kill PB because we'll start it in background
-    -- after screen on
-    if (eventType == hs.caffeinate.watcher.screensDidSleep) then
-        print("Killing PB")
-        hs.execute([[pkill PB\ for\ Desktop]])
-    end
+    -- if (eventType == hs.caffeinate.watcher.screensDidWake) then
+    --     print("staring PB")
+    --     hs.execute([[open --background -a  PB\ for\ Desktop]])
+    -- end
+    -- -- After screen sleep we kill PB because we'll start it in background
+    -- -- after screen on
+    -- if (eventType == hs.caffeinate.watcher.screensDidSleep) then
+    --     print("Killing PB")
+    --     hs.execute([[pkill PB\ for\ Desktop]])
+    -- end
 end
 
 sleepWatcher = hs.caffeinate.watcher.new(caffeinateWatcher)
