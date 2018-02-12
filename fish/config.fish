446567e01886d2100470206397802bd556e1fa83
Marcus van Bergen <marcusvanbergen@live.com>
2018-02-12 17:01:46 +0100

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,15 @@ set -x -g PATH $PATH ~/bin /usr/local/sbin
 #for mvn ECP
 set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home"
 
+#set home for anaconda
+#set -x  PATH /usr/local/anaconda3/bin $PATH
+
+#python2 of homebrew
+#export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+#jupyter notebook salias
+alias jn='jupyter notebook'
+
 #maven aliases
 alias mci='mvn clean install -T 1C -Dlockdown=true'
 alias mcis='mvn clean install -T 1C -DskipTests'
@@ -24,3 +33,5 @@ alias mcios='mvn clean install -o -T 1C -DskipTests'
 alias mi='mvn install -T 1C -Dlockdown=true'
 alias mio='mvn install -o -T 1C -Dlockdown=true'
 alias mios='mvn clean install -o -T 1C -DskipTests'
+
+
