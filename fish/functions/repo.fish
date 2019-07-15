2ef57e719f8a072e793e20f47423d44f4448ddbf
Marcus van Bergen <mbhjvanbergen@gmail.com>
2019-07-15 23:35:43 +0200

    Added repo's from @rkalis with a deeper search!

--- fish/functions/repo.fish
+++ fish/functions/repo.fish
@@ -1,5 +1,5 @@
 function repo
-    set -l repo_path (find ~/repos -mindepth 1 -maxdepth 1 -type d -name "*$argv*" | head -n 1)
-    echo "found $repo_path"
+    set -l repo_path (repodir $argv)
+    echo "$repo_path"
     cd "$repo_path"
 end
