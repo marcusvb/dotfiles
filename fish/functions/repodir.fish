f51e05d192ea3ae305ef61438ff2ef9892d89439
Marcus van Bergen <mbhjvanbergen@gmail.com>
2019-07-16 11:42:30 +0200

    Allows searching for folders at depth 1

--- fish/functions/repodir.fish
+++ fish/functions/repodir.fish
@@ -1,6 +1,6 @@
 function repodir
     set repo_base ~/repos
-    set repo_path (find "$repo_base" -mindepth 2 -maxdepth 10 -type d -name "*$argv*" | head -n 1)
+    set repo_path (find "$repo_base" -mindepth 1 -maxdepth 10 -type d -name "*$argv*" | head -n 1)
     if not test "$argv"; or not test "$repo_path"
         set repo_path "$repo_base"
     end
