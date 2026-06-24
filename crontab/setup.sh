ecd80f65611fbf80702b8466e6e62df25537f0c1
Marcus van Bergen <mbhjvanbergen@gmail.com>
2026-06-24 22:35:08 +0200

    remove cronmail & add package

--- crontab/setup.sh
+++ crontab/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-(crontab -l 2>/dev/null; echo '@reboot /Users/marcus/repos/dotfiles/crontab/backup_keepass.sh > /dev/null 2>&1') | sort -u | crontab -
+(crontab -l 2>/dev/null; echo '@reboot /Users/marcus/repos/dotfiles/crontab/backup_keepass.sh > /dev/null 2>&1
+') | sort -u | crontab -
